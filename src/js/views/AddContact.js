5f7d5c50d6b516cd836aba8218ff9870205038f0
tuquepantin <tuque250@gmail.com>
2023-05-26 21:33:10 +0000

    bt

--- src/js/views/AddContact.js
+++ src/js/views/AddContact.js
@@ -6,14 +6,12 @@ import { Context } from "../store/appContext";
 import "../../styles/demo.css";
 
 export const AddContact = () => {
-	const { store, actions } = useContext(Context);
 
-	const handleChange = (event) => {
-		setNewContact({
-		  ...newContact,
-		  [event.target.name]: event.target.value,
-		});
-	  };
+	const { store, actions } = useContext(Context);
+  
+  const handleChange = (event) => {
+		setNewContact({...newContact,[event.target.name]: event.target.value,});
+	};
 
 	const [newContact, setNewContact] = useState({
 		full_name: "",
@@ -21,49 +19,61 @@ export const AddContact = () => {
 			agenda_slug: "victorpantin",
 			address:"",
 			phone:""
-	  });
-
-
-	const handleSubmit = (event) => {
+	});
+  
+  const handleSubmit = (event) => {
 		event.preventDefault();
 		actions.createContact(newContact)
-	  };
+	};
 
 	return (
 		<>
-		   <form onSubmit={handleSubmit}>
+		<form onSubmit={handleSubmit}>
+    <div class="mb-3">
+        <label class="form-label">Full Name</label>
         <input
+          class="form-control"
           type="text"
           name="full_name"
           placeholder="name"
           value={newContact.full_name}
           onChange={handleChange}
         />
+    </div>
+    <div class="mb-3">  
+        <label class="form-label">Email</label>
         <input
+          class="form-control"
           type="email"
           name="email"
           placeholder="Email"
           value={newContact.email}
           onChange={handleChange}
         />
+    </div>
+    <div class="mb-3">   
+        <label class="form-label">Phone</label>
         <input
+          class="form-control"
           type="text"
           name="phone"
           placeholder="Phone"
           value={newContact.phone}
           onChange={handleChange}
         />
+    </div>   
+    <div class="mb-3">
+        <label class="form-label">Address</label>
         <input
+          class="form-control"
           type="text"
           name="address"
           placeholder="Address"
           value={newContact.address}
           onChange={handleChange}
         />
-        
-        
-        
-        <button type="submit">Add Contact</button>
+    </div>    
+        <button class="btn btn-primary" type="submit">Save</button>
       </form> 
 		</>
 	);
